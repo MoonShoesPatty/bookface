9b2be9dfecdd8672d8c49b12a0cec1077041fb5b
Pat Johnston <pattrashgarbage@gmail.com>
2018-01-15 21:31:12 -0500

    Styled nav bar

--- dev/scripts/components/mainFeed.js
+++ dev/scripts/components/mainFeed.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-    BrowserRouter as Router,
-    Route, Link
+	BrowserRouter as Router,
+	Route, Link
 } from 'react-router-dom';
 
 class MainFeed extends React.Component {
-    render() {
-        return (
-            <main>
-                <p>Main Feed.</p>
-            </main>
-        )
-    }
+	render() {
+		return (
+			<main>
+				<p>Book<span>Face</span></p>
+			</main>
+		)
+	}
 }
 
 export default MainFeed;
--- dev/scripts/components/profilePage.js
+++ dev/scripts/components/profilePage.js
@@ -10,6 +10,23 @@ class ProfilePage extends React.Component {
         return (
             <main>
                 <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
+                <p>Profile Page.</p>
             </main>
         )
     }
